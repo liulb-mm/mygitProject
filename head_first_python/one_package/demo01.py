6cbad410f97733e706a24663bc6eaefe2bd8f3c0
liulb-mm <liulb89@163.com>
2020-06-01 15:25:26 +0800

    函数

--- head_first_python/one_package/demo01.py
+++ head_first_python/one_package/demo01.py
@@ -56,12 +56,57 @@ phrase = "Don't panic!"
 plist = list(phrase)
 print(phrase)
 print(plist)
-new_plist=''.join(plist[1:3])
-new_plist=new_plist+''.join([plist[5],plist[4],plist[7],plist[6]])
+new_plist = ''.join(plist[1:3])
+new_plist = new_plist + ''.join([plist[5], plist[4], plist[7], plist[6]])
 print(new_plist)
 
-#字典
-dict={'a': 1, 'b': '可以啊', '姓名': '练习册', '可以': 33}
-#遍历字典
-for k in dict:
-    print(k,dict[k])
+# 字典
+dict = {'a': 1, 'b': '可以啊', '姓名': '练习册', '可以': 33}
+# 遍历字典
+# for k in dict:
+#     print(k,dict[k])
+# 按照升序顺序遍历
+for k in sorted(dict):
+    print(k, dict[k])
+# 常用遍历字典的方法，dict.items()
+for k, v in dict.items():
+    print(k, v)
+
+fruits = {}
+fruits["apple"] = 2
+if 'bananas' in fruits:
+    fruits['bananas'] += 1
+else:
+    fruits['bananas'] = 1
+print(fruits)
+
+for k, v in fruits.items():
+    print(k, v)
+
+# 集合set，集合的合并用union,sorted是对集合进行升序排序 >并集
+set_1 = {'a', 'e', 'i', 'o', 'u'}
+set_2 = {'a', 'b', 'c', 'd', 'e'}
+set_all = sorted(set_1.union(set_2))
+print(set_all)
+word = "hahahahnihaoha"
+set_all = set_1.union(set(word))
+print(set_all)
+# 使用difference查看不是共有元素 >差集
+set_diff = set_1.difference(set_2)
+print(set_diff)
+
+# 使用intersection查询共同对象 >交集
+set_same = set_1.intersection(set_2)
+print(set_same)
+
+
+# 告诉传入参数需要哥字符串类型，返回字符串类型
+def zhujie(word: str) -> str:
+    return word
+
+
+zhujie("你好啊")
+
+#取两个字符串的交集，返回一个集合类型
+def search(phrase: str, leetter: str) -> set:
+    return set(leetter).intersection(set(phrase))
