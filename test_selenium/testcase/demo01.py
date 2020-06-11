c8d5869f3ebc4a1e6773ed7cb280733161647086
liulb-mm <liulb89@163.com>
2020-06-11 15:00:44 +0800

    testcase

---
+++ test_selenium/testcase/demo01.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestWebdriver:
+    def setup(self):
+        self.driver=webdriver.Chrome()
+        self.driver.get("https://www.testerhome.com")
+        self.driver.implicitly_wait(10)
+
+    def test_case(self):
+        self.driver.find_element(By.LINK_TEXT,"MTSC2020 中国互联网测试开发大会议题征集").click()
